da04cfb569f1945df367e9ef8a67704d4cf44e61
Italo Ferreira <grd788@gmail.com>
2021-12-24 17:15:55 -0300

    Inicialização do Projeto e Tabelas

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express, { response }  from "express";
+
+
+const app = express()
+
+
+app.get("/", (request, response) => {
+  return response.json({
+    message: "Hello World"
+  })
+}) 
+
+app.listen(3333, () => {
+  console.log("Server is running in port 3333")
+})
