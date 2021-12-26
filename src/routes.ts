67de4c242d56d41019361f506e7e861e6ae427a8
Italo Ferreira <grd788@gmail.com>
2021-12-26 17:52:13 -0300

    Middleware authenticação e criação do pedido Delivery

--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,26 @@
  
 import { Router } from "express";
+import { ensureAuthenticateClient } from "./middlewares/ensureAuthenticateClient";
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientContrller";
+import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router()
 
-const createClientController = new CreateClientController
-const authenticateClientController = new AuthenticateClientController
-const createDeliverymanController = new CreateDeliverymanController
-const authenticareDeliverymanController = new AuthenticateDeliverymanController
+const createClientController = new CreateClientController()
+const authenticateClientController = new AuthenticateClientController()
+const createDeliverymanController = new CreateDeliverymanController()
+const authenticareDeliverymanController = new AuthenticateDeliverymanController()
+const createDeliveryController = new CreateDeliveryController()
 
 routes.post("/client/",  createClientController.handle)
 routes.post("/deliveryman/",  createDeliverymanController.handle)
 
 routes.post("/client/login", authenticateClientController.handle)
 routes.post("/deliveryman/login", authenticareDeliverymanController.handle)
+
+routes.post("/delivery", ensureAuthenticateClient, createDeliveryController.handle)
 
 export { routes }
